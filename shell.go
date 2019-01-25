a2a9fe938509668516162e8c1136010ec8785277
Dmitry <spalf@spalf.ru>
2019-01-25 12:04:58 +0300

    Update shell.go

--- shell.go
+++ shell.go
@@ -8,8 +8,8 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/gorillalabs/go-powershell/backend"
-	"github.com/gorillalabs/go-powershell/utils"
+	"github.com/Spalf/go-powershell/backend"
+	"github.com/Spalf/go-powershell/utils"
 	"github.com/juju/errors"
 )
 
